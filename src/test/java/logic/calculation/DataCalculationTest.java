e2568edcc366cbe58645f8f274cbb22b7e8cde70
151250150 <151250150@smail.nju.edu.cn>
2017-03-08 20:20:17 +0800

    测试一个方法。写mock我想原地爆炸

--- src/test/java/logic/calculation/DataCalculationTest.java
+++ src/test/java/logic/calculation/DataCalculationTest.java
@@ -1,13 +1,26 @@
 package logic.calculation;
 
+import logic.tools.DateHelper;
+import org.junit.Before;
 import org.junit.Test;
+import vo.StockVO;
 
-import static org.junit.Assert.*;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Mark.W on 2017/3/7.
  */
 public class DataCalculationTest {
+
+    private DataCalculation dataCalculation;
+
+    @Before
+    public void setUp() {
+        this.dataCalculation = new DataCalculation();
+    }
+
     @Test
     public void getStockInfo() throws Exception {
 
@@ -15,7 +28,16 @@ public class DataCalculationTest {
 
     @Test
     public void getMarketInfo() throws Exception {
+        Date startDate = DateHelper.stringTransToDate("3/1/17");
+        Date endDate = DateHelper.stringTransToDate("3/14/17");
+        StockVO stockVO = this.dataCalculation.getStockInfo("1", startDate, endDate);
 
+        assertEquals("1", stockVO.stockCode);
+
+        assertEquals(6, stockVO.minValue, 0);
+        assertEquals(320, stockVO.maxValue, 0);
+
+        assertEquals(10, stockVO.stockDailyInfoVOs.size());
     }
 
 }
