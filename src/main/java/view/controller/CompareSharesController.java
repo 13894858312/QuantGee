f68367356aa772487a9456bd5b37e198891cba57
151250149 <151250149@smail.nju.edu.cn>
2017-03-14 20:19:38 +0800

    还是小错误

--- src/main/java/view/controller/CompareSharesController.java
+++ src/main/java/view/controller/CompareSharesController.java
@@ -187,17 +187,17 @@ public class CompareSharesController {
 
     @FXML
     private void showRightDayLine() {
-        drawLeftAveLine(AverageLineType.DAYS_10);
+        drawRightAveLine(AverageLineType.DAYS_10);
     }
 
     @FXML
     private void showRightQuarterLine() {
-        drawLeftAveLine(AverageLineType.DAYS_60);
+        drawRightAveLine(AverageLineType.DAYS_60);
     }
 
     @FXML
     private void showRightYearLine() {
-        drawLeftAveLine(AverageLineType.DAYS_240);
+        drawRightAveLine(AverageLineType.DAYS_240);
     }
 
     @FXML
