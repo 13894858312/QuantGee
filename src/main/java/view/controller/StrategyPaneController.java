1a7d92e7a5a240175e2e53f3464799bfbd42fa72
151250149 <151250149@smail.nju.edu.cn>
2017-04-05 19:59:56 +0800

    修改迭代一fxml（删除按钮），完善策略展示界面fxml，修改maincss

--- src/main/java/view/controller/StrategyPaneController.java
+++ src/main/java/view/controller/StrategyPaneController.java
@@ -1,8 +1,53 @@
 package view.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 /**
  * 展示策略的三个图
  * Created by wangxue on 2017/3/31.
  */
 public class StrategyPaneController {
+
+    @FXML
+    private AnchorPane graphA;
+    @FXML
+    private AnchorPane graphB_0;
+    @FXML
+    private AnchorPane graphB_1;
+    @FXML
+    private AnchorPane graphC;
+
+    @FXML
+    private Label year;
+    @FXML
+    private Label base;
+
+    @FXML
+    private Label sharp;
+    @FXML
+    private Label alpha;
+    @FXML
+    private Label beta;
+    @FXML
+    private Label volatility;
+    @FXML
+    private Label message;
+    @FXML
+    private Label max;
+    @FXML
+    private Label turnover;
+
+    @FXML
+    private Label positive;
+    @FXML
+    private Label negative;
+    @FXML
+    private Label win;
+
+
+    public void init(){
+
+    }
 }
