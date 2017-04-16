56df0608d8d08c51c56fbb6455111d876b9e43f5
151250197 <151250197@smail.nju.edu.cn>
2017-04-16 20:45:42 +0800

    修改图表

--- src/main/java/view/UI/WinningStrategy.java
+++ src/main/java/view/UI/WinningStrategy.java
@@ -14,31 +14,31 @@ import java.util.ArrayList;
 public class WinningStrategy extends Pane {
     public WinningStrategy(boolean judge, ArrayList<AbnormalReturnGraphDataVO> abnormalReturnGraphDataVOArrayList, int width, int height){
 
-//        this.getStylesheets().add("/css/logYield_Compare.css");
+        this.getStylesheets().add("/css/logYield_Compare.css");
         //x-xAxis
         final NumberAxis xAxis = new NumberAxis();
         xAxis.setLabel("Date");
 
         //y-yAxis
         final NumberAxis yAxis = new NumberAxis();
-        yAxis.setLabel("ExcessEarning Yield");
+        yAxis.setLabel("WinningStrategy Yield");
 
         /* chart */
         final AreaChart<Number, Number> lc = new AreaChart<Number, Number>(xAxis, yAxis);
         lc.setCreateSymbols(false);
         lc.setPrefSize(width, height);
-        lc.setTitle("ExcessEarning Yield Chart");
+        lc.setTitle("WinningStrategy Yield Chart");
 
         //data
         XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();
         series.setName("Date");
-        if(judge) {
-            for (int i = abnormalReturnGraphDataVOArrayList.size() - 1; i >= 0; i--) {
+        if(!judge) {
+            for (int i = 0; i <abnormalReturnGraphDataVOArrayList.size(); i++) {
                 AbnormalReturnGraphDataVO abnormalReturnGraphDataVO = abnormalReturnGraphDataVOArrayList.get(i);
                 series.getData().add(new XYChart.Data<Number, Number>(abnormalReturnGraphDataVO.holdingPeriod, abnormalReturnGraphDataVO.stategyWinRate));
             }
         }else{
-            for (int i = abnormalReturnGraphDataVOArrayList.size() - 1; i >= 0; i--) {
+            for (int i = 0; i <abnormalReturnGraphDataVOArrayList.size(); i++) {
                 AbnormalReturnGraphDataVO abnormalReturnGraphDataVO = abnormalReturnGraphDataVOArrayList.get(i);
                 series.getData().add(new XYChart.Data<Number, Number>(abnormalReturnGraphDataVO.returnPeriod, abnormalReturnGraphDataVO.stategyWinRate));
             }
