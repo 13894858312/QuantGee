44c4def721356de00978a7d36da57ee744cbcf12
151250002 <151250002@smail.nju.edu.cn>
2017-04-17 15:26:32 +0800

    MeanReversionStrategy的完成

--- src/main/java/logic/strategy/MeanReversionStrategy.java
+++ src/main/java/logic/strategy/MeanReversionStrategy.java
@@ -2,10 +2,15 @@ package logic.strategy;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 
+import data.StockData;
+import dataDao.StockDataDao;
 import logic.calculation.GraphCalculation;
 import logic.tools.AverageLineType;
 import logic.tools.DateHelper;
+import logic.tools.MathHelper;
+import logic.tools.SwitchAverageLineType;
 import po.StockPO;
 import vo.AverageLineVO;
 
@@ -15,7 +20,8 @@ import vo.AverageLineVO;
  */
 public class MeanReversionStrategy implements Strategy {
 	
-	private ArrayList<ArrayList<AverageLineVO>> allAverageLine;
+	//保存所有股票的N日移动均线
+	private ArrayList<HashMap<String, AverageLineVO>> allAverageLine;
 
     @Override
     public ArrayList<StockYield> initHoldingStocks(StockPool stockPool) {
@@ -41,7 +47,7 @@ public class MeanReversionStrategy implements Strategy {
     		return null;
     	}
     	
-    	//遍历股票池中数据最多的股票
+    	//遍历股票池中股票
         for(int i=0; i<stockPool.getStockInfos().size(); ++i) {
             StockPO before = stockPool.getStockInfos().get(i).getBeforeStockPO();
             String beforeDate = before.getDate();
@@ -51,18 +57,18 @@ public class MeanReversionStrategy implements Strategy {
             	continue;
             }
             
-            GraphCalculation calculation = new GraphCalculation();
             
-            //初始化每一只股票的N日移动均线
-            ArrayList<AverageLineVO> averageLine = calculation.getAverageLineInfoByCode(DateHelper.getInstance().stringTransToDate(beforeDate)
+            //初始化第i只股票的N日移动均线
+            HashMap<String, AverageLineVO> averageLine = getAverageLineInfoByCode(DateHelper.getInstance().stringTransToDate(beforeDate)
             		, stockPool.getEndDate(), yesterday.getStockCode(), type);
 
             //添加到allAverageLine
             allAverageLine.add(averageLine);
             
 
-            //计算偏离度，(均值-前一天的开盘价)/ 均值
-            double yield = (yesterday.getADJ()-before.getADJ())/before.getADJ();
+            //计算偏离度，(均值-当天的开盘价)/ 均值
+            double average = averageLine.get(DateHelper.getInstance().dateTransToString(stockPool.getStartDate())).averageValue; 
+            double yield = (average-yesterday.getADJ())/average;
 
             stockYields.add(new StockYield(yesterday.getStockCode(), yield));
         }
@@ -72,6 +78,71 @@ public class MeanReversionStrategy implements Strategy {
 
     @Override
     public ArrayList<StockYield> rebalanceHoldingStocks(StockPool stockPool, Date beforeDate, Date today) {
+    	
+    	ArrayList<StockYield> stockYields = new ArrayList<>();
+
+        for(int i=0; i<stockPool.getStockInfos().size(); ++i) {
+            StockPO before = stockPool.getStockInfos().get(i).getStockByDate(beforeDate);
+            StockPO yesterday = stockPool.getStockInfos().get(i).getStockByDate(today);
+
+            if(yesterday != null && before != null) {
+                //计算偏离度，(均值-当天的开盘价)/ 均值
+            	double average = allAverageLine.get(i).get(DateHelper.getInstance().dateTransToString(today)).averageValue;
+                double yield = (average-yesterday.getADJ())/average;
+
+                stockYields.add(new StockYield(yesterday.getStockCode(), yield));
+            }
+
+        }
+        
         return null;
+    }
+    
+    
+    /**
+     * 辅助方法，获取N日移动均线
+     * @param startDate
+     * @param endDate
+     * @param stockCode
+     * @param averageLineType
+     * @return
+     */
+    public HashMap<String, AverageLineVO> getAverageLineInfoByCode(Date startDate, Date endDate, String stockCode, AverageLineType averageLineType) {
+
+        assert (stockCode != null && !stockCode.equals("") && startDate != null && endDate != null)
+                : "logic.calculation.GraphCalculation.getAverageLineInfoByCode参数异常";
+        
+        StockDataDao stockDataDao = new StockData();
+
+        //获取均线图的时间间隔
+        int dayNums = SwitchAverageLineType.getDayInterval(averageLineType);
+        if(dayNums > DateHelper.getInstance().calculateDaysBetween(startDate, endDate)) {
+            return null;
+        }
+
+        //如果总天数小于均线图的时间间隔出错
+        ArrayList<StockPO> stockPOS = stockDataDao.getStockPOsByTimeInterval(DateHelper.getInstance().dateTransToString(startDate),
+                DateHelper.getInstance().dateTransToString(endDate), stockCode);
+
+        if(dayNums > stockPOS.size()) {
+            return null;
+        }
+
+       HashMap<String, AverageLineVO> result = new HashMap<>();
+       
+       for(int i=stockPOS.size()-dayNums; i>=0; i--) {
+    	   Date date = DateHelper.getInstance().stringTransToDate(stockPOS.get(i).getDate());
+    	   
+    	   double all = 0;
+    	   for(int j=i+dayNums-1; j>i-1; j--) {
+    		   all += stockPOS.get(j).getClosePrice();
+    	   }
+    	   
+    	   double average = MathHelper.formatData(all/dayNums, 2);
+    	   
+    	   result.put(stockPOS.get(i).getDate(),new AverageLineVO(stockPOS.get(i).getStockCode(), stockPOS.get(i).getStockName(),averageLineType, date, average));
+       }
+       
+       return result;
     }
 }
