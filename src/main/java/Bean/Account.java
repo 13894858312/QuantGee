8041fe8680a70f687b4fd19a198bb6848c23f930
151250002 <151250002@smail.nju.edu.cn>
2017-05-05 21:24:52 +0800

    add java bean

---
+++ src/main/java/Bean/Account.java
@@ -0,0 +1,25 @@
+package Bean;
+
+/**
+ * Created by PC on 2017/5/5.
+ */
+public class Account {
+
+    private String accountID;
+    private String password;
+    
+	public String getAccountID() {
+		return accountID;
+	}
+	public void setAccountID(String accountID) {
+		this.accountID = accountID;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+}
---
+++ src/main/java/Bean/News.java
@@ -0,0 +1,10 @@
+package Bean;
+
+/**
+ * Created by PC on 2017/5/5.
+ */
+public class News {
+
+    private String title;
+
+}
