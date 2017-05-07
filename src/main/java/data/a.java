6b371dcee14988eb65ed1819597d23e9417060f5
151250149 <151250149@smail.nju.edu.cn>
2017-05-07 20:15:52 +0800

    Input包

--- src/main/java/data/a.java
+++
@@ -1,7 +0,0 @@
-package data;
-
-/**
- * Created by wangxue on 2017/5/5.
- */
-public class a {
-}
---
+++ src/main/java/input/HistogramInput.java
@@ -0,0 +1,12 @@
+package input;
+
+/**
+ * Created by wangxue on 2017/5/7.
+ */
+public class HistogramInput {
+
+    public HistogramInput(){
+
+    }
+
+}
