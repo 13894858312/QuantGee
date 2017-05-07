d812a9ca68f772de62204b75d905b5cc288b8cbb
151250149 <151250149@smail.nju.edu.cn>
2017-05-07 20:47:27 +0800

    DAO

--- src/main/java/DAO/strategyDAO/StrategyDAO.java
+++ src/main/java/DAO/strategyDAO/StrategyDAO.java
@@ -1,7 +1,21 @@
 package DAO.strategyDAO;
 
+import input.StrategyInput;
+import logic.strategy.Strategy;
+
+import java.util.ArrayList;
+
 /**
  * Created by wangxue on 2017/5/5.
  */
 public interface StrategyDAO {
+
+    public boolean addStrategy(Strategy strategy);
+
+    public boolean removeStrategy(String strategyID);
+
+    public Strategy getStrategy(String strategyID);
+
+    public ArrayList<Strategy> getUSerStrategy(String userID);
+
 }
--- src/main/java/DAO/tradeDAO/TradeDAO.java
+++ src/main/java/DAO/tradeDAO/TradeDAO.java
@@ -1,7 +1,16 @@
 package DAO.tradeDAO;
 
+import bean.Trade;
+
+import java.util.ArrayList;
+
 /**
  * Created by wangxue on 2017/5/5.
  */
 public interface TradeDAO {
+
+    public boolean addTradeInfo(Trade trade);
+
+    public ArrayList<Trade> getUserTradeList(String userID);
+
 }
