fcba6139dbf8c191b30c703857f4850040c9e69a
151250149 <151250149@smail.nju.edu.cn>
2017-05-12 19:32:37 +0800

    ???

--- src/main/java/po/UserAnalysisDataPO.java
+++ src/main/java/po/UserAnalysisDataPO.java
@@ -3,16 +3,16 @@ package po;
 import java.util.Map;
 
 public class UserAnalysisDataPO {
-	
+
+    private int userNum;								//总的用户数量
 	private Map<String, Integer> registerNumByTime;		//按时间获取注册数量
 
-	public Map<String, Integer> getRegisterNumByTime() {
+    public Map<String, Integer> getRegisterNumByTime() {
 		return registerNumByTime;
 	}
 
 	public void setRegisterNumByTime(Map<String, Integer> registerNumByTime) {
 		this.registerNumByTime = registerNumByTime;
 	}
-
 	
 }
--- src/main/java/po/test.java
+++
@@ -1,7 +0,0 @@
-package po;
-
-/**
- * Created by wangxue on 2017/5/11.
- */
-public class test {
-}
