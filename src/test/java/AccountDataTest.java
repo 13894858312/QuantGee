39cd61cf764f08030e014518ef4b02dc9d2c4c5f
151250149 <151250149@smail.nju.edu.cn>
2017-05-15 22:09:36 +0800

    test

--- src/test/java/AccountDataTest.java
+++ src/test/java/AccountDataTest.java
@@ -1,7 +1,16 @@
 import DAO.accountDAO.AccountDAO;
 import bean.Account;
 import data.accountData.AccountData;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+import org.hibernate.service.ServiceRegistry;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * Created by wangxue on 2017/5/14.
@@ -29,4 +38,31 @@ public class AccountDataTest {
         accountDAO.updateAccount(account);
     }
 
+    @Test
+    public void getTxT(){
+        try{
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            configuration.addClass(Account.class);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            Session session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+
+            String hql = "SELECT m.stockID FROM MarketInfo m";
+            Query query = session.createQuery(hql);
+            List<String> list = query.list();
+
+            for(String l:list){
+                System.out.print(l+",");
+            }
+
+            transaction.commit();
+            session.close();
+            sessionFactory.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
