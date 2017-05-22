c9d75b3559acd7a1941763c058dec4f18b7c46c3
151250150 <151250150@smail.nju.edu.cn>
2017-05-22 11:38:38 +0800

    bcy再尝试一下

--- src/main/java/logic/stock/StockDataCalculation.java
+++ src/main/java/logic/stock/StockDataCalculation.java
@@ -1,7 +1,14 @@
 package logic.stock;
 
+import bean.Stock;
+import logic.tools.DateHelper;
+import logic.tools.MathHelper;
 import org.springframework.stereotype.Service;
+import vo.stock.MAVO;
 import vo.stock.StockHistoricalVO;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by Mark.W on 2017/5/16.
@@ -41,6 +48,53 @@ public class StockDataCalculation {
         }
 
         return new KLineData(positive, upper, lower);
+    }
+
+    /**
+     * 均线图是将每天的收盘价加权平均,从而得到一条带有趋势性的轨迹。
+     * 在日 K 线图中一般白线、黄线、紫线、绿线、蓝线依次分别表示:5、10、20、30、60日移动平均线。
+     * 移动平均线常用线有 5 天、10 天、30 天、60 天、120 天和 240 天的指标。
+     * 其中，5天和10天的短期移动平均线。是短线操作的参照指标，称做日均线指标;
+     * 30 天和 60 天的是中期均线指标，称做季均线指标;
+     * 120 天、240 天的是长期均 线指标，称做年均线指标。
+     * @param sourceStocks 股票信息
+     * @param period 均线周期
+     * @return
+     */
+//    public ArrayList<MAVO> getAverageLineInfoByCode(String stockCode, String startDate, String endDate, int period) {
+//    if(period <= 0 || period > DateHelper.calculateDaysBetween(startDate, endDate)) {
+//        return null;
+//    }
+
+    public ArrayList<MAVO> getAverageLineInfoByCode(Iterator<Stock> sourceStocks, int period) {
+
+        //如果总天数小于均线图的时间间隔出错
+        ArrayList<Stock> stocks = new ArrayList<>();
+        while(sourceStocks.hasNext()) {
+            stocks.add(sourceStocks.next());
+        }
+
+        if(period >= stocks.size()) {
+            return null;
+        }
+
+        ArrayList<MAVO> result = new ArrayList<>();
+        Stock stock;
+
+        for (int i = period-1; i<stocks.size(); ++i){
+            stock = stocks.get(i);
+
+            double all = 0;
+            for(int j=i-period+1; j<i+1; ++j) {
+                all += stocks.get(j).getClose();
+            }
+
+            double average = MathHelper.formatData(all/(double)period, 4);
+
+            result.add(new MAVO(stock.getStockId(),period, stock.getDate(), average));
+        }
+
+        return result;
     }
 
 }
