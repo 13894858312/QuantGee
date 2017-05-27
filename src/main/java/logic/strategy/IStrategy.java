1ee0f1ebd9015cf4e079683ee6d6319aaf906345
151250150 <151250150@smail.nju.edu.cn>
2017-05-27 19:06:24 +0800

    注释错误

--- src/main/java/logic/strategy/IStrategy.java
+++ src/main/java/logic/strategy/IStrategy.java
@@ -7,20 +7,20 @@ import java.util.ArrayList;
  */
 public interface IStrategy {
     /**
-     * 在第一次运行时 确定持有的股票
-     * @param stockPool 股票池
-     * @param dates 下一个持有期每天的日期
-     * @return ArrayList<StockYield>
-     */
-    ArrayList<StockYield> initHoldingStocks(StockPool stockPool, ArrayList<String> dates);
-
-    /**
-     * 调仓时计算股票池所有指标量，用来确定持有的股票
-     * @param stockPool 股票池
-     * @param beforeDate 形成期第一天的时间
-     * @param today 调仓当天的时间
-     * @param dates 下一个持有期每天的日期
-     * @return ArrayList<StockYield>
-     */
-    ArrayList<StockYield> rebalanceHoldingStocks(StockPool stockPool, String beforeDate, String today, ArrayList<String> dates);
+//     * 在第一次运行时 确定持有的股票
+//     * @param stockPool 股票池
+//     * @param dates 下一个持有期每天的日期
+//     * @return ArrayList<StockYield>
+//     */
+//    ArrayList<StockYield> initHoldingStocks(StockPool stockPool, ArrayList<String> dates);
+//
+//    /**
+//     * 调仓时计算股票池所有指标量，用来确定持有的股票
+//     * @param stockPool 股票池
+//     * @param beforeDate 形成期第一天的时间
+//     * @param today 调仓当天的时间
+//     * @param dates 下一个持有期每天的日期
+//     * @return ArrayList<StockYield>
+//     */
+//    ArrayList<StockYield> rebalanceHoldingStocks(StockPool stockPool, String beforeDate, String today, ArrayList<String> dates);
 }
