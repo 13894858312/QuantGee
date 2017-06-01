77c67777c9c7f79facfd8c7c3e5427ecd7e46890
151250002 <151250002@smail.nju.edu.cn>
2017-06-01 15:24:30 +0800

    标签获取list，数据库连接失败

--- src/main/java/action/LoginAction.java
+++ src/main/java/action/LoginAction.java
@@ -2,55 +2,76 @@ package action;
 
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import service.user.AccountService;
 import vo.user.AccountVO;
 
+import java.util.ArrayList;
+
 @Controller
-public class LoginAction extends ActionSupport{
+public class LoginAction extends ActionSupport {
 
-//	private AccountVO accountVO = new AccountVO(); // 表单中的姓名
-	private AccountVO accountVO; // 表单中的姓名
-
-	@Autowired
-	private AccountService accountService;
-
-	public String login() {
-
-		if(accountService.login(accountVO)) {
-			return SUCCESS;
-		} else {
-			return "fail";
-		}
-
-	}
-	public String logout() {
-		System.out.println("yes");
-		if(accountService.login(accountVO)) {
-			return SUCCESS;
-		} else {
-			return "fail";
-		}
-
-	}
-
-	public String register(){
-
-		if (accountService.register(accountVO)) {
-			return SUCCESS;
-		}else {
-			return "fail";
-		}
-	}
+    //	private AccountVO accountVO = new AccountVO(); // 表单中的姓名
+    private AccountVO accountVO; // 表单中的姓名
+    private ArrayList<String> testStrings;
 
 
-	public AccountVO getAccountVO() {
-		return accountVO;
-	}
+    @Autowired
+    private AccountService accountService;
 
-	public void setAccountVO(AccountVO accountVO) {
-		this.accountVO = accountVO;
-	}
+    //	@Result({
+//			@Result(name = "success" ,value = "/view/ucenter/user_center.jsp"),
+//			@Result(name = "fail" ,value = "/view/ucenter/login.jsp")
+//	})
+    public String login() {
+        testStrings = new ArrayList<>();
+        testStrings.add("bcy");
+        testStrings.add("gzy");
+        testStrings.add("love");
+//		if(accountService.login(accountVO)) {
+        return SUCCESS;
+//		} else {
+//			return "fail";
+//		}
+
+    }
+
+    public String logout() {
+        System.out.println("yes");
+        if (accountService.login(accountVO)) {
+            return SUCCESS;
+        } else {
+            return "fail";
+        }
+
+    }
+
+    public String register() {
+
+        if (accountService.register(accountVO)) {
+            return SUCCESS;
+        } else {
+            return "fail";
+        }
+    }
+
+
+    public AccountVO getAccountVO() {
+        return accountVO;
+    }
+
+    public void setAccountVO(AccountVO accountVO) {
+        this.accountVO = accountVO;
+    }
+
+    public ArrayList<String> getTestStrings() {
+        return testStrings;
+    }
+
+    public void setTestStrings(ArrayList<String> testStrings) {
+        this.testStrings = testStrings;
+    }
 
 }
