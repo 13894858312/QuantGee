f21e0ec185240ddd8d0f9da67b26f62de3b298ec
151250197 <151250197@smail.nju.edu.cn>
2017-06-07 19:17:42 +0800

    新的菜单栏

---
+++ web/js/marketInfo/headcontroller.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Administrator on 2017/6/7.
+ */
+function changewhole1() {
+    var ha1 = document.getElementById("ha1");
+    var a1 = document.getElementById("a1");
+    a1.style.color = "rgb(62, 196, 131)";
+    ha1.style.borderBottom = "2px solid rgb(62, 196, 131)";
+
+}
+function changewhole2() {
+    var ha2 = document.getElementById("ha2");
+    var a2 = document.getElementById("a2");
+    a2.style.color = "rgb(62, 196, 131)";
+    ha2.style.borderBottom = "2px solid rgb(62, 196, 131)";
+
+}
+function changewhole3() {
+    var ha3 = document.getElementById("ha3");
+    var a3 = document.getElementById("a3");
+    a3.style.color = "rgb(62, 196, 131)";
+    ha3.style.borderBottom = "2px solid rgb(62, 196, 131)";
+
+}
+function changewhole4() {
+    var ha4 = document.getElementById("ha4");
+    var a4 = document.getElementById("a4");
+    a4.style.color = "rgb(62, 196, 131)";
+    ha4.style.borderBottom = "2px solid rgb(62, 196, 131)";
+
+}
+function changewhole5() {
+    var ha5 = document.getElementById("ha5");
+    var a5 = document.getElementById("a5");
+    a5.style.color = "rgb(62, 196, 131)";
+    ha5.style.borderBottom = "2px solid rgb(62, 196, 131)";
+}
