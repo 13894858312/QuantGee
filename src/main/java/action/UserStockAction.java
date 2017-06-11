90028f21c22abf20364436c663e7db650f7d02c2
151250002 <151250002@smail.nju.edu.cn>
2017-06-11 19:06:38 +0800

    修改ucenter，成功获取收藏股票，添加欢迎标签

---
+++ src/main/java/action/UserStockAction.java
@@ -0,0 +1,25 @@
+package action;
+
+import vo.stock.StockCurrentVO;
+
+import java.util.ArrayList;
+
+/**
+ * Created by duoduogao on 17/6/11.
+ */
+public class UserStockAction {
+
+    private ArrayList<StockCurrentVO> stockCurrentVOS;
+
+//    public
+
+
+    public ArrayList<StockCurrentVO> getStockCurrentVOS() {
+        return stockCurrentVOS;
+    }
+
+    public void setStockCurrentVOS(ArrayList<StockCurrentVO> stockCurrentVOS) {
+        this.stockCurrentVOS = stockCurrentVOS;
+    }
+
+}
